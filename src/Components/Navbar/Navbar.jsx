8dd13de9bd72c7f68d17d43f200eda2d459a8029
Facundo Romano <facundo-romano@hotmail.com>
2023-11-09 14:39:00 -0300

    menu-added responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,36 @@
+import { useState } from 'react';
 import './Navbar.css';
-
-import logo from '../../assets/logoVanguard.png'
+import logo from '../../assets/logoVanguard.png';
+import { HiOutlineBars3 } from "react-icons/hi2";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-      <>
-        <nav className="navbar">
+    <>
+      <nav className="navbar">
         <ul className="navbar-logo">
-            <a className="navbar-item" href="/">
-                <img className='logo' src={logo}/>
-                <h2 className='title-logo'>ANGUARD</h2>
-            </a>
+          <a className="navbar-item" href="/">
+            <img className="logo" src={logo} alt="Logo" />
+            <h2 className="title-logo">ANGUARD</h2>
+          </a>
         </ul>
-            <div className="navbar-list">
-                <div className="navbar-item"><a id='right-navbar-items' href="/#artworks">Artworks</a></div>
-                <div className="navbar-item"><a id='right-navbar-items' href="/#about">About</a></div>
-                <div className="navbar-item"><a id='right-navbar-items' href="/#artist">Artist</a></div>
-            </div>
-        </nav>
-      </>
+        <div className={`navbar-list ${isMenuOpen ? 'active' : ''}`}>
+          <div className="navbar-item"><a id="right-navbar-items" href="/#artworks">Artworks</a></div>
+          <div className="navbar-item"><a id="right-navbar-items" href="/#about">About</a></div>
+          <div className="navbar-item"><a id="right-navbar-items" href="/#artist">Artist</a></div>
+        </div>
+        <div className="menu-button" onClick={toggleMenu}>
+          <div className={`menu-icon ${isMenuOpen ? 'active' : ''}`}>
+            <HiOutlineBars3/>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
